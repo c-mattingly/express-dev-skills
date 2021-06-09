5ddc005cc219eec7b9c8d0592148f6a64bdd008c
Mike Mattingly <cmattingly1584@gmail.com>
2021-06-09 14:56:15 -0700

    adds add skill, delete skill functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,20 +2,36 @@ const Skill = require("../models/skill");
 
 module.exports = {
     index,
-    show
+    show,
+    new: newSkill,
+    create,
+    delete: deleteSkill
 }
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+}
+
+function create(req, res) {
+    Skill.create(req.body);
+    res.redirect('/skills');
+}
+
+function newSkill(req, res) {
+    res.render('skills/new');
+};
 
 function index(req, res) {
     const skillsList = Skill.getAll();
 
     res.render("skills/index", {
-        skills: skillsList
-    })
+        skills: Skill.getAll()
+    });
 }
 
 function show(req, res) {
-    const skill = Skill.getOne(req.params.id);
     res.render("skills/show", {
-        skill: skill
+        skill: Skill.getOne(req.params.id)
     })
 }
--- models/skill.js
+++ models/skill.js
@@ -5,12 +5,25 @@ const skills = [
 ];
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne
 };
+
+function deleteOne(id) {
+    const idx = skills.findIndex(skill => skill.id === parseInt(id));
+    skills.splice(idx, 1);
+}
 function getAll() {
     return skills;
 };
 
 function getOne(id) {
     return skills.find(skill => skill.id === parseInt(id));
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.done = false;
+    skills.push(skill);
 }
